94692883669b45d24e6fcc76d51a8c200f9aa6b8
Guilherme <guilhermemendescsgo@gmail.com>
2025-04-30 15:14:31 -0300

    adicionando arma

--- Assets/Scripts/Player/PlayerAbilityShoot.cs
+++ Assets/Scripts/Player/PlayerAbilityShoot.cs
@@ -5,26 +5,39 @@ using UnityEngine.InputSystem;
 
 public class PlayerAbilityShoot : PlayerAbilityBase
 {
+
     public GunBase gunBase;
+    public Transform gunPosition;
+
+    private GunBase _currentGun;
 
     protected override void Init()
     {
         base.Init();
 
+        CreateGun();
+
         inputs.Gameplay.Shoot.performed += cts => StartShoot();
         inputs.Gameplay.Shoot.canceled += cts => CancelShoot();
     }
 
+    private void CreateGun()
+    {
+        _currentGun = Instantiate(gunBase, gunPosition);
+
+        _currentGun.transform.position = _currentGun.transform.localEulerAngles = Vector3.zero;
+    }
+
     private void StartShoot()
     {
-        gunBase.StartShoot();
+        _currentGun.StartShoot();
         Debug.Log("Start Shoot");
     }
 
     private void CancelShoot()
     {
         Debug.Log("Cancel Shoot");
-        gunBase.StopShoot();
+        _currentGun.StopShoot();
     }
 }
 
